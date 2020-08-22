4d49ca12683855193914ab6c5ea33cdb9f6c16d2
R L <rpnospam@gmail.com>
2020-08-21 19:29:11 -0700

    Adding files for code train 2-5.

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const Datastore = require('nedb');
+
+const app = express();
+app.listen(8080, () => { console.log('listening...'); });
+app.use(express.static('./public'));
+app.use(express.json({ limit: '1mb' }));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.post('/api', (request, response) => {
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data); // sends data back to client
+});
+
+app.get('/api', (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
+});
+
