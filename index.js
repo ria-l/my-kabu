c571ad64df14be7d3ccfed68a581e1a4042f2fc5
R L <rpnospam@gmail.com>
2020-08-23 17:43:33 -0700

    Cleaned up working ver.

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
 const express = require('express');
-const Datastore = require('nedb');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
 const tiingo_api_key = process.env.TIINGO_API_KEY;
 const app = express();
-app.listen(8080, () => { console.log('listening...'); });
+app.listen(8080, () => {
+  console.log('listening...');
+});
 app.use(express.static('./public'));
 app.use(express.json({ limit: '1mb' }));
-
-const database = new Datastore('database.db');
-database.loadDatabase();
-
-app.get('/api', (request, response) => {
-  database.find({}, (err, data) => {
-    if (err) {
-      response.end();
-      return;
-    }
-    response.json(data);
-  });
-});
-
-app.post('/api', (request, response) => {
-  const data = request.body;
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
-  database.insert(data);
-  response.json(data); // sends data back to client
-});
 
 // proxy server
 app.get('/ticker/:ticker', async (request, response) => {
   const ticker = request.params.ticker;
-  const api_url = `https://api.tiingo.com/tiingo/daily/${ticker}?token=${tiingo_api_key}`;
-  const fetch_response = await fetch(api_url);
-  const json = await fetch_response.json();
+  const apiUrl = `https://api.tiingo.com/tiingo/daily/${ticker}?token=${tiingo_api_key}`;
+  const response = await fetch(apiUrl);
+  const json = await response.json();
   response.json(json);
 });
 
@@ -43,8 +23,8 @@ app.get('/daily/:ticker', async (request, response) => {
   const startDate = '2020-08-01';
   const endDate = '2020-08-22';
   const ticker = request.params.ticker;
-  const api_url = `https://api.tiingo.com/tiingo/daily/${ticker}/prices?startDate=${startDate}&endDate=${endDate}&token=${tiingo_api_key}`;
-  const fetch_response = await fetch(api_url);
-  const json = await fetch_response.json();
+  const apiUrl = `https://api.tiingo.com/tiingo/daily/${ticker}/prices?startDate=${startDate}&endDate=${endDate}&token=${tiingo_api_key}`;
+  const response = await fetch(apiUrl);
+  const json = await response.json();
   response.json(json);
 });
--- public/index.js
+++ public/index.js
@@ -1,71 +1,60 @@
 document.querySelector('#submit').addEventListener('click', () => {
-  // getTiingo();
   chartIt();
 });
 
-async function getTiingo() {
-  const tick = document.querySelector('#ticker').value.toUpperCase();
-  const api_url = `/ticker/${tick}`;
-  const response = await fetch(api_url);
-  const json = await response.json();
-
-  console.log(json);
-  const root = document.querySelector('#ticker-info');
-
-root.textContent=''
-  for (const property in json) {
-    const p = document.createElement('p');
-
-    if (property == 'detail') {
-      p.textContent = `Ticker "${tick}" not found.`;
-      root.append(p);
-    } else {
-      p.textContent = `${property}: ${json[property]}`;
-      root.append(p);
-    }
-  }
-  
-  
-  // dailies
-  const api_daily_url = `/daily/${tick}`;
-  const response_daily = await fetch(api_daily_url);
-  const json_daily = await response_daily.json();
-  const xAxis = [];
-  const yAxis = [];
-  json_daily.forEach((e) => {
-    xAxis.push(e.date.split('T')[0]);
-    yAxis.push(e.close);
-  });
-  return {xAxis, yAxis};
-};
-
-
 async function chartIt() {
-  const json = await getTiingo();
-
+  const tiingoData = await getTiingo();
   const ctx = document.getElementById('myChart').getContext('2d');
-  const myChart = new Chart(ctx, {
+  new Chart(ctx, {
     type: 'line',
     data: {
-      labels: json.xAxis,
-      datasets: [{
-        label: '$$',
-        data: json.yAxis,
-        backgroundColor: ['rgba(255, 159, 64, 0.2)'],
-        borderColor: ['rgba(255, 159, 64, 1)'],
-        borderWidth: 1,
-        lineTension: 0
-      }]
+      labels: tiingoData.xAxisLabels,
+      datasets: [
+        {
+          label: `${tiingoData.ticker}: ${tiingoData.name}`,
+          data: tiingoData.yAxisLabels,
+          backgroundColor: ['rgba(0, 200, 5, 0.2)'],
+          borderColor: ['rgba(0, 200, 5, 1)'],
+          borderWidth: 1,
+          lineTension: 0,
+        },
+      ],
     },
     options: {
       responsive: true,
-      // maintainAspectRatio: true
-    }
+    },
   });
 }
 
-// async function getData() {
-//   const response = await fetch('/api');
-//   const data = await response.json();
-// }
-// getData();
+async function getTiingo() {
+  const ticker = document.querySelector('#ticker').value.toUpperCase();
+  const metadataApiUrl = `/ticker/${ticker}`;
+  const metadataResponse = await fetch(metadataApiUrl);
+  const metadataJson = await metadataResponse.json();
+
+  const root = document.querySelector('#ticker-info');
+  root.textContent = '';
+  for (const property in metadataJson) {
+    const p = document.createElement('p');
+    if (property === 'detail') {
+      p.textContent = `Ticker "${ticker}" not found.`;
+      root.append(p);
+    } else {
+      p.textContent = `${property}: ${metadataJson[property]}`;
+      root.append(p);
+    }
+  }
+  const { name } = metadataJson;
+
+  const dailyApiUrl = `/daily/${ticker}`;
+  const dailyResponse = await fetch(dailyApiUrl);
+  const dailyJson = await dailyResponse.json();
+  const xAxisLabels = [];
+  const yAxisLabels = [];
+  dailyJson.forEach((e) => {
+    xAxisLabels.push(e.date.split('T')[0]);
+    yAxisLabels.push(e.close);
+  });
+
+  return { xAxisLabels, yAxisLabels, ticker, name };
+}
