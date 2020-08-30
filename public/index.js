7b933cfbed7e119cdc0ba999aeca4ba3ba3ef042
R L <rpnospam@gmail.com>
2020-08-30 15:10:06 -0700

    refactor overloaded function

--- public/index.js
+++ public/index.js
@@ -1,18 +1,24 @@
-document.querySelector('#submit').addEventListener('click', () => {
-  chartIt();
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#submit').addEventListener('click', () => {
+    const ticker = document.querySelector('#ticker').value.toUpperCase();
+    chartIt(ticker);
+    renderStockMetadata(ticker);
+  });
 });
 
-async function chartIt() {
-  const tiingoData = await getTiingo();
+async function chartIt(ticker) {
+  const chartData = await getDataForChart(ticker);
   const ctx = document.getElementById('myChart').getContext('2d');
   new Chart(ctx, {
     type: 'line',
     data: {
-      labels: tiingoData.xAxisLabels,
+      labels: chartData.xAxisLabels,
       datasets: [
         {
-          label: `${tiingoData.ticker}: ${tiingoData.name}`,
-          data: tiingoData.yAxisLabels,
+          label: `${chartData.ticker}: ${chartData.name}`,
+          data: chartData.yAxisLabels,
           backgroundColor: ['rgba(0, 200, 5, 0.2)'],
           borderColor: ['rgba(0, 200, 5, 1)'],
           borderWidth: 1,
@@ -26,12 +32,33 @@ async function chartIt() {
   });
 }
 
-async function getTiingo() {
-  const ticker = document.querySelector('#ticker').value.toUpperCase();
+async function getDataForChart(ticker) {
+  const dailyApiUrl = `/daily/${ticker}`;
+  const dailyResponse = await fetch(dailyApiUrl);
+  const dailyJson = await dailyResponse.json();
+  const xAxisLabels = [];
+  const yAxisLabels = [];
+  for (let i = 0; i < dailyJson.length; i++) {
+    xAxisLabels.push(dailyJson[i].date.split('T')[0]);
+    yAxisLabels.push(dailyJson[i].close);
+  }
+
+  const metaData = await getStockMetadata(ticker);
+  const name = metaData['name'];
+
+  return { xAxisLabels, yAxisLabels, ticker, name };
+}
+
+async function getStockMetadata(ticker) {
   const metadataApiUrl = `/ticker/${ticker}`;
   const metadataResponse = await fetch(metadataApiUrl);
   const metadataJson = await metadataResponse.json();
 
+  return metadataJson;
+}
+
+async function renderStockMetadata(ticker) {
+  const metadataJson = await getStockMetadata(ticker);
   const root = document.querySelector('#ticker-info');
   root.textContent = '';
   for (const property in metadataJson) {
@@ -44,17 +71,6 @@ async function getTiingo() {
       root.append(p);
     }
   }
-  const { name } = metadataJson;
-
-  const dailyApiUrl = `/daily/${ticker}`;
-  const dailyResponse = await fetch(dailyApiUrl);
-  const dailyJson = await dailyResponse.json();
-  const xAxisLabels = [];
-  const yAxisLabels = [];
-  dailyJson.forEach((e) => {
-    xAxisLabels.push(e.date.split('T')[0]);
-    yAxisLabels.push(e.close);
-  });
-
-  return { xAxisLabels, yAxisLabels, ticker, name };
 }
+
+module.exports = getDataForChart;
