808bbe312e3d75bc75ceb77d421cb854251a2553
R L <rpnospam@gmail.com>
2020-10-18 19:58:14 -0700

    Add "delete" functionality to stock list

--- src/StockList.js
+++ src/StockList.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import * as Utilities from './utilities';
 
 class StockList extends Component {
-  // state = {};
+  state = { changed: false };
+
+  handleDeleteRequest = (id) => {
+    Utilities.deleteLotFromPortfolio(id);
+    this.setState({ changed: true });
+  };
 
   render() {
     const rows = [];
     const portfolio = JSON.parse(window.localStorage.getItem('portfolio'));
     if (portfolio) {
       for (const lot in portfolio.lots) {
-        rows.push(<StockListRow lot={lot} key={lot} />);
+        rows.push(
+          <StockListRow
+            lot={lot}
+            key={lot}
+            onDeleteRequest={this.handleDeleteRequest}
+          />
+        );
       }
     }
 
@@ -18,6 +29,8 @@ class StockList extends Component {
         <table>
           <tbody>
             <tr>
+              {/* // TODO: Delete 'ID" later */}
+              <th>ID</th>
               <th>Stock</th>
               <th>Graph</th>
               <th>Today's close</th>
@@ -26,6 +39,7 @@ class StockList extends Component {
               <th>Market Value</th>
               <th>Daily Gain</th>
               <th>Total Gain</th>
+              <th>Delete</th>
             </tr>
             {rows}
           </tbody>
@@ -36,18 +50,24 @@ class StockList extends Component {
 }
 
 class StockListRow extends Component {
+  handleClick = (id) => {
+    this.props.onDeleteRequest(id);
+  };
+
   render() {
     const portfolio = JSON.parse(window.localStorage.getItem('portfolio'));
     const symbol = portfolio.lots[this.props.lot].symbol;
+    const id = portfolio.lots[this.props.lot].id;
     const numShares = Utilities.getNumberOfShares(this.props.lot);
     const todaysPrice = Utilities.getTodaysPrice(symbol);
     const yesterdaysPrice = Utilities.getYesterdaysPrice(symbol);
     const yesterdaysValue = numShares * yesterdaysPrice;
     const todaysValue = numShares * todaysPrice;
-    console.log(symbol, todaysPrice);
 
     return (
       <tr>
+        {/* ID TODO: delete later */}
+        <td>{id}</td>
         {/* Stock */}
         <td>{symbol}</td>
         {/* Graph */}
@@ -76,6 +96,9 @@ class StockListRow extends Component {
         </td>
         {/* Total gain */}
         <td></td>
+        <td>
+          <button onClick={() => this.handleClick(id)}>Delete</button>
+        </td>
       </tr>
     );
   }
