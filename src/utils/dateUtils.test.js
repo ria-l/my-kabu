f9074217af9ba2c59e756232a603d2657f60be13
ria-l <55597349+ria-l@users.noreply.github.com>
2021-01-27 22:57:29 -0800

    remove console.log

--- src/utils/dateUtils.test.js
+++ src/utils/dateUtils.test.js
@@ -5,7 +5,6 @@ describe('setDateToUtcMidnight', () => {
     const date = new Date(
       'Fri Jan 01 2021 17:24:29 GMT-0800 (Pacific Standard Time)'
     );
-    console.log(date);
     expect(dateUtils.setDateToUtcMidnight(date)).toEqual(
       new Date('2021-01-01T00:00:00.000Z')
     );
@@ -13,7 +12,6 @@ describe('setDateToUtcMidnight', () => {
 
   it('datePicker', () => {
     const date = new Date('2020-12-10T20:00:00.000Z');
-    console.log(date);
     expect(dateUtils.setDateToUtcMidnight(date)).toEqual(
       new Date('2020-12-10T00:00:00.000Z')
     );
