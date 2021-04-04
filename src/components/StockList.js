34f5cbcfb95f91489010c022aef6f30920c0db0c
ria-l <55597349+ria-l@users.noreply.github.com>
2021-04-03 23:26:17 -0700

    use hooks

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,81 +1,80 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { StockListRow } from './StockListRow';
 import { EditableStockListRow } from './EditableStockListRow';
 import * as portfolioUtils from '../utils/portfolioUtils';
 
-class StockList extends Component {
-  state = { rerender: false, editing: false };
+const StockList = (props) => {
+  const [rerender, setRerender] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [id, setId] = useState();
 
-  deleteRow = (id) => {
+  const deleteRow = (id) => {
     portfolioUtils.deleteLotFromPortfolio(id);
-    this.props.rerender();
+    props.rerender();
   };
 
-  editRow = (id) => {
-    this.setState({ editing: true, id });
+  const editRow = (id) => {
+    setEditing(true);
+    setId(id);
   };
 
-  finishEditingRow = (id, ticker, boughtShares) => {
+  const finishEditingRow = (id, ticker, boughtShares) => {
     if (id) {
       portfolioUtils.updatePortfolio(id, ticker, boughtShares);
     }
-    this.setState({ editing: false, rerender: true });
-    this.props.rerender();
+    setEditing(false);
+    setRerender(true);
+    props.rerender();
   };
 
-  render() {
-    const rows = [];
-    const portfolio = JSON.parse(window.localStorage.getItem('portfolio'));
-    if (portfolio) {
-      for (const lot in portfolio.lots) {
-        if (
-          this.state.editing === true &&
-          portfolio.lots[lot].id === this.state.id
-        ) {
-          rows.push(
-            <EditableStockListRow
-              key={portfolio.lots[lot].id}
-              lot={lot}
-              onDelete={this.deleteRow}
-              onSaveOrCancel={this.finishEditingRow}
-            />
-          );
-        } else {
-          rows.push(
-            <StockListRow
-              key={portfolio.lots[lot].id}
-              lot={lot}
-              onDelete={this.deleteRow}
-              onEdit={this.editRow}
-            />
-          );
-        }
+  const rows = [];
+  const portfolio = JSON.parse(window.localStorage.getItem('portfolio'));
+  if (portfolio) {
+    for (const lot in portfolio.lots) {
+      if (editing === true && portfolio.lots[lot].id === id) {
+        rows.push(
+          <EditableStockListRow
+            key={portfolio.lots[lot].id}
+            lot={lot}
+            onDelete={deleteRow}
+            onSaveOrCancel={finishEditingRow}
+          />
+        );
+      } else {
+        rows.push(
+          <StockListRow
+            key={portfolio.lots[lot].id}
+            lot={lot}
+            onDelete={deleteRow}
+            onEdit={editRow}
+          />
+        );
       }
     }
-
-    return (
-      <div className="main">
-        <table id="stocklist">
-          <tbody>
-            <tr>
-              <th>ID</th>
-              <th>Stock</th>
-              <th>Buy Date</th>
-              <th>Today's close</th>
-              <th>Change since...</th>
-              <th>Shares</th>
-              <th>Market Value</th>
-              <th>Daily Gain</th>
-              <th>Total Gain</th>
-              <th>Delete</th>
-              <th>Edit</th>
-            </tr>
-            {rows}
-          </tbody>
-        </table>
-      </div>
-    );
   }
-}
+
+  return (
+    <div className="main">
+      <table id="stocklist">
+        <tbody>
+          <tr>
+            <th>ID</th>
+            <th>Stock</th>
+            <th>Buy Date</th>
+            <th>Today's close</th>
+            <th>Change since...</th>
+            <th>Shares</th>
+            <th>Market Value</th>
+            <th>Daily Gain</th>
+            <th>Total Gain</th>
+            <th>Delete</th>
+            <th>Edit</th>
+          </tr>
+          {rows}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default StockList;
